e3197de37ee0216f73e0917604d60057947568ef
Linton Junior <linton@motorola.com>
2023-05-03 16:13:13 -0400

    sensors

---
+++ camera/src/main/java/ru/mirea/egorovakv/camera/MainActivity.java
@@ -0,0 +1,108 @@
+package ru.mirea.egorovakv.camera;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import ru.mirea.egorovakv.camera.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    private static final int CAMERA_REQUEST = 0;
+    private boolean isWork = false;
+    private Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //permissions check
+        int cameraPermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.
+                WRITE_EXTERNAL_STORAGE);
+
+        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus
+                == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+            Log.d("onCreate", "permissions granted");
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
+        }
+
+        // Создание функции обработки результата от системного приложения «камера»
+        ActivityResultCallback<ActivityResult> callback = new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    binding.imageView.setImageURI(imageUri);
+                }
+            }
+        };
+        ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                callback);
+
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // проверка на наличие разрешений для камеры
+                if (isWork) {
+                    try {
+                        File photoFile = createImageFile();
+                        // генерирование пути к файлу на основе authorities
+                        String authorities = getApplicationContext().getPackageName() + ".fileprovider";
+                        imageUri = FileProvider.getUriForFile(MainActivity.this, authorities, photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                        cameraActivityResultLauncher.launch(cameraIntent);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            isWork = grantResults.length > 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED;
+            Log.d("onRequestPermissionsResult", "permissions granted");
+        }
+    }
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDirectory);
+    }
+
+}
